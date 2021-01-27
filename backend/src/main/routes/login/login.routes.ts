a5ef58ce883e975ae37507c63e8f32d0cbd18c80
Biscoitinhoo <gustavo.dickmann123@gmail.com>
2021-01-27 18:33:47 -0300

    user login working properly

--- backend/src/main/routes/login/login.routes.ts
+++ backend/src/main/routes/login/login.routes.ts
@@ -6,9 +6,9 @@ const usersRouter = Router();
 
 usersRouter.post("/", async (request, response) => {
   try {
-    const { email, password } = request.body;
-
     const authenticateUserController = new AuthenticateUserController();
+
+    const { email, password } = request.body;
     const authenticatedUser = await authenticateUserController.authenticateUser(
       {
         email,
@@ -16,7 +16,13 @@ usersRouter.post("/", async (request, response) => {
       }
     );
 
-    return response.json({ authenticatedUser });
+    return response.json(
+      "Welcome to the system, " +
+        authenticatedUser.firstName +
+        ". " +
+        "Your favorite thing is " +
+        authenticatedUser.favoriteThing
+    );
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
